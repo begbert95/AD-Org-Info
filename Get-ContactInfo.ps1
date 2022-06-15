3395d102ca54d2e2c5c13cfc034463491cea956c
defenestrateddefense <begbert95@gmail.com>
2022-06-15 16:30:11 -0600

    Update Get-ContactInfo.ps1

--- Get-ContactInfo.ps1
+++ Get-ContactInfo.ps1
@@ -1,8 +1,8 @@
-$UserList = Get-ADUser -Filter '*' -SearchBase $_ -Properties name, displayname, samaccountname, title, office, EmailAddress, officephone, telephonenumber, mobile, mobilephone, manager | Select-Object name, displayname, samaccountname, title, office, EmailAddress, officephone, telephonenumber, mobile, mobilephone, manager}
+$UserList = Get-ADUser -Filter '*' -Properties name, displayname, samaccountname, title, office, EmailAddress, officephone, telephonenumber, mobile, mobilephone, manager | Select-Object name, displayname, samaccountname, title, office, EmailAddress, officephone, telephonenumber, mobile, mobilephone, manager}
 
 
 $ExportList = foreach($user in $UserList) {
-    $l2man= Get-ADUser $user.manager -Properties extensionattribute2 | Select-Object extensionattribute2
+    $l2man= Get-ADUser $user.manager -Properties samaccountname | Select-Object samaccountname
     $PSCustomObject = @{
         "Legal Name" = $user.name
         "Display Name" = $user.displayname
@@ -15,8 +15,8 @@ $ExportList = foreach($user in $UserList) {
         "Mobile" = $user.mobile
         "Mobile Phone" = $user.mobilephone
         "L1 Manager" = $user.manager
-        "L2 Manager" = $l2man
+        "L2 Manager" = $l2man.samaccountname
     }
     $PSCustomObject
 }
-$ExportList | Select-Object "Legal Name", "Display Name", "User Name","Title", "Office", "Email", "Office Phone", "Telephone", "Mobile", "Mobile Phone", "L1 Manager", "L2 Manager" | Export-Csv c:\mudge.csv
+$ExportList | Select-Object "Legal Name", "Display Name", "User Name", "Title", "Office", "Email", "Office Phone", "Telephone", "Mobile", "Mobile Phone", "L1 Manager", "L2 Manager" | Export-Csv c:\export.csv
